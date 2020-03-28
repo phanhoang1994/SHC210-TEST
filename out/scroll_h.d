44076c0da919b7c7c5c6498324c31e25037e7454
Phan Van Hoang <vanhoang@tne.vn>
2020-03-28 11:25:44 +0700

    add shc210 test

---
+++ out/scroll_h.d
@@ -0,0 +1,9 @@
+.\out\scroll_h.o: scroll_h.c
+.\out\scroll_h.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\out\scroll_h.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdbool.h
+.\out\scroll_h.o: ..\THU VIEN TIVAWARE\TivaWare_C_Series-2.1.4.178\inc/hw_memmap.h
+.\out\scroll_h.o: ..\THU VIEN TIVAWARE\TivaWare_C_Series-2.1.4.178\driverlib/gpio.h
+.\out\scroll_h.o: inc/function.h
+.\out\scroll_h.o: ..\THU VIEN TIVAWARE\TivaWare_C_Series-2.1.4.178\inc/hw_types.h
+.\out\scroll_h.o: inc/display.h
+.\out\scroll_h.o: inc/global.h
